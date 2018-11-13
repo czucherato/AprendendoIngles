a7eed97f4eaf98204b48cbc611c8ee21216daf39
Carlos Alberto Zucherato <carlos.zucheratto@gmail.com>
2018-11-13 11:50:37 -0200

    Atualizado arquivo app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './componentes/app.component';
 import { TopoComponent } from './componentes/topo/topo.component';
 import { PainelComponent } from './componentes/painel/painel.component';
+import { ProgressoComponent } from './componentes/progresso/progresso.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopoComponent,
-    PainelComponent
+    PainelComponent,
+    ProgressoComponent
   ],
   imports: [
     BrowserModule
