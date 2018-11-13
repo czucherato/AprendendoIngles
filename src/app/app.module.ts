a7a57fbff3d63eac2349b6deb02c186983a2cc1c
Carlos Alberto Zucherato <carlos.zucheratto@gmail.com>
2018-11-13 11:45:03 -0200

    Adicionado o componente tentativas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './componentes/app.component';
 import { TopoComponent } from './componentes/topo/topo.component';
 import { PainelComponent } from './componentes/painel/painel.component';
+import { TentativasComponent } from './componentes/tentativas/tentativas.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopoComponent,
-    PainelComponent
+    PainelComponent,
+    TentativasComponent
   ],
   imports: [
     BrowserModule
