7d328a689234db9bc84aedd2bdbbe69a2e3b5ec1
Carlos Alberto Zucherato <carlos.zucheratto@gmail.com>
2018-11-13 14:34:22 -0200

    Adicionado bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './componentes/app.component';
 import { TopoComponent } from './componentes/topo/topo.component';
 import { PainelComponent } from './componentes/painel/painel.component';
-<<<<<<< HEAD
 import { TentativasComponent } from './componentes/tentativas/tentativas.component';
-=======
 import { ProgressoComponent } from './componentes/progresso/progresso.component';
->>>>>>> progresso
 
 @NgModule({
   declarations: [
-  ApppComponent,
+    AppComponent,
     TopoComponent,
     PainelComponent,
     TentativasComponent,
